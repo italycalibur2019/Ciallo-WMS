fcc68b8d6710413d88acf0c3ce51b051f6885e45
dhr <920893925@qq.com>
2025-02-23 10:11:24 +0800

    first commit

---
+++ src/test/java/com/italycalibur/ciallo/warehouse/CialloWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italycalibur.ciallo.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CialloWmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
